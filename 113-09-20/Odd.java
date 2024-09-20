34381d5455ee03d40cfddaf030c7cd40b220f827
essman6969 <163495986+essman6969@users.noreply.github.com>
2024-09-20 12:03:20 +0800

    113

---
+++ 113-09-20/Odd.java
@@ -0,0 +1,15 @@
+package cc.openhome;
+
+public class Odd {
+    public static void main(String[] args) {
+        var input = 10;
+        var remain = input % 2; 
+        
+        if(remain == 1) {
+            System.out.printf("%d �O�_��%n", input); 
+        } 
+        else {
+            System.out.printf("%d �O����%n", input); 
+        }
+    }
+}
