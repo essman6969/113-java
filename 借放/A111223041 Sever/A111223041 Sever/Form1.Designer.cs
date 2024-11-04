f1935ebf4f8da042ce19a27bcf94bc6e2012cf42
essman6969 <A111223041@mail.shu.edu.tw>
2024-11-04 11:58:42 +0800

    借放

---
+++ 借放/A111223041 Sever/A111223041 Sever/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace A111223041_Sever
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(15, 251);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(373, 172);
+            this.listBox2.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label4.Location = new System.Drawing.Point(12, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 19);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "傳送的訊息";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label3.Location = new System.Drawing.Point(234, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 19);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "線上使用者";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 50);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "啟動伺服器端";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "2019";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label2.Location = new System.Drawing.Point(36, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 19);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "PORT:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14F);
+            this.label1.Location = new System.Drawing.Point(36, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "IP:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(173, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 88);
+            this.listBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 443);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "許嘉祥的server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 借放/A111223041 Sever/A111223041 Sever/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A111223041_Sever
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TcpListener Server;
+        Socket Client;
+        Thread Th_Svr;
+        Thread Th_Clt;
+        Hashtable HT = new Hashtable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = MyIP();
+        }
+        private string MyIP()
+        {
+            string hn = Dns.GetHostName();
+            IPAddress[] ip = Dns.GetHostEntry(hn).AddressList;
+            foreach (IPAddress it in ip)
+            {
+                if (it.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return it.ToString();
+                }
+            }
+            return "";
+        }
+        
+        private void ServerSub()
+        {
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            Server = new TcpListener(EP);
+            Server.Start(100);
+            while (true)
+            {
+                Client = Server.AcceptSocket();
+                Th_Clt = new Thread(Listen);
+                Th_Clt.IsBackground = true;
+                Th_Clt.Start();
+            }
+
+        }
+        private void Listen()
+        {
+            Socket Sck = Client;
+            Thread Th = Th_Clt;
+            while (true)
+            {
+                try
+                {
+                    byte[] B = new byte[1023];
+                    int inLen = Sck.Receive(B);
+                    String Msg = Encoding.Default.GetString(B, 0, inLen);
+                    listBox2.Items.Add("(接收)" + Msg);
+                    string Cmd = Msg.Substring(0, 1);
+                    string Str = Msg.Substring(1);
+                    switch (Cmd)
+                    {
+                        case "0":
+                            if (listBox1.Items.IndexOf(Str) < 0)
+                            {
+                                HT.Add(Str, Sck);
+                                listBox1.Items.Add(Str);
+                                SendAll(OnlineList());
+                            }
+                            else
+                            {
+                                byte[] R = Encoding.Default.GetBytes("3" + "使用者名稱重複");
+                                Sck.Send(R, 0, R.Length, SocketFlags.None);
+                                Th.Abort();
+                            }
+                            break;
+                        case "9":
+                            HT.Remove(Str);
+                            listBox1.Items.Remove(Str);
+                            SendAll(OnlineList());
+                            Th.Abort();
+                            break;
+                        case "1":
+                            SendAll(Msg);
+                            break;
+                        default:
+                            string[] C = Str.Split('|');
+                            SendTo(Cmd + C[0], C[1]);
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+        private string OnlineList()
+        {
+            String L = "L";
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                L += listBox1.Items[i];
+                if (i < listBox1.Items.Count - 1)
+                    L += ",";
+            }
+            return L;
+        }
+        private void SendTo(string Str, string User)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str);
+            Socket Sck = (Socket)HT[User];
+            Sck.Send(B, 0, B.Length, SocketFlags.None);
+            listBox2.Items.Add("(傳送給)" + User + ":" + Str);
+        }
+        private void SendAll(string Str)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str);
+            foreach (Socket s in HT.Values)
+            {
+                s.Send(B, 0, B.Length, SocketFlags.None);
+            }
+        }
+
+        
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Th_Svr = new Thread(ServerSub);
+            Th_Svr.IsBackground = true;
+            Th_Svr.Start();
+            button1.Enabled = false;
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            Application.ExitThread();
+        }
+    }
+}
